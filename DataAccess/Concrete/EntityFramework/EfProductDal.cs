0621a6b0bcaeb824fff29c154aa1f913c9aaadaf
Yaren Kasımoğlu <70938435+yaren-kasimoglu@users.noreply.github.com>
2021-03-23 15:08:21 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,59 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //NuGet
+    public class EfProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            using (NorthwindContext context=new NorthwindContext())
+            {
+                var addedEntity = context.Entry(entity);// referansı yakala
+                addedEntity.State = EntityState.Added;//eklenecek nesne olduğunu göster
+                context.SaveChanges(); // ekle
+            }
+        }
+
+        public void Delete(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges(); 
+            }
+        }
+
+        public Product Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;//update
+                context.SaveChanges();
+            }
+        }
+    }
+}
