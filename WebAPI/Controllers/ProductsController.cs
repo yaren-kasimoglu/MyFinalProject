bc912477be727390dc7bd49d45d28a1ea8f54b5e
Yaren Kasımoğlu <70938435+yaren-kasimoglu@users.noreply.github.com>
2021-05-01 14:45:16 +0300

    Committed

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]  //attribute bir class ile ilgili bilgi verme-imzalama yöntemi --- java da annotation
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService;
+
+        //Loosely coupled : gevşek bağlılık
+        //IoC Container: Inversion of Control
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            //Dependency chain:  bağımlılık zinciri
+
+           
+            var result = _productService.GetAll();
+
+           return result.Data;
+        }
+    }
+}
